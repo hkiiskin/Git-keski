0bbd9f4b89eb016a256cdcc6ac63aa97db990aca
Hannu <hannu.kiiskinen@pp.phnet.fi>
2022-12-30 14:53:09 +0200

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
 print("Hello world!")
+<<<<<<< HEAD
+print("Hello darkness my old friend")
+=======
+print("Hello darkness my old friend")
+print("I've come to talk with you again")
+>>>>>>> feature/modify-print
